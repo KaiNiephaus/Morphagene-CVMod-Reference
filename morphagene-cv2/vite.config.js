55b00d3c41065cb16aa90f4b4943f1a403071d0f
Kai Niephaus <kaiuwe.niephaus@gmail.com>
2026-04-12 16:01:19 +0200

    Update vite.config.js

--- morphagene-cv2/vite.config.js
+++ morphagene-cv2/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change to your GitHub repo name before deploying, e.g. '/morphagene-cv/'
-// Use '/' if deploying to a custom domain root
-const BASE_PATH = '/morphagene-cv/'
-
 export default defineConfig({
-  base: BASE_PATH,
+  base: '/',
   plugins: [react()],
 })
